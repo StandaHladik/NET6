3fd182310caeb6101a1973451192339abe7dc530
StandaHladik <Stanislav.Hladik@saint-gobain.com>
2021-10-04 12:15:18 +0200

    Hello jmeno

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,16 +6,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Hello();
+            //Console.WriteLine("Hello World!");
+            Hello("Stando");
             //int sum = Add(5, 3);
-            //Console.Write(sum);
-            AddAndPrint(95, 55);
+            //Console.WriteLine(sum);
+            //AddAndPrint(95, 55);
         }
 
-        static void Hello()
+        static void Hello(string jmeno)
         {
-            Console.WriteLine("Hello!");
+            Console.WriteLine($"Hello {jmeno} !");
         }
 
         static int Add(int a, int b)
@@ -23,14 +23,15 @@ namespace HelloWorld
             return a + b;
         }
 
-        static void AddAndPrint(int a, int b)
+        static int AddAndPrint(int a, int b)
         {
             int sum;
 
             sum = a + b;
 
-            Console.WriteLine(sum);
+            Console.WriteLine($"Součet a + b: {a} + {b} = {sum}");
 
+            return sum;
         }
     }
 }
