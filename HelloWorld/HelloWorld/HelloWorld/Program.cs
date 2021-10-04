6f5042fe4c4a85d49d3fa80559c70a3abf8ac0da
StandaHladik <Stanislav.Hladik@saint-gobain.com>
2021-10-04 12:01:02 +0200

    AddAndPrint

--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,30 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Hello();
+            //int sum = Add(5, 3);
+            //Console.Write(sum);
+            AddAndPrint(95, 55);
+        }
+
+        static void Hello()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void AddAndPrint(int a, int b)
+        {
+            int sum;
+
+            sum = a + b;
+
+            Console.WriteLine(sum);
+
         }
     }
 }
