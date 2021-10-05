e441bb0613973cbd690e0e906cdf681c880b80c0
Stanislav Hladik <Stanislav.Hladik@saint-gobain.com>
2021-10-05 09:55:46 +0200

    GetDayName

---
+++ HelloWorld/HelloWorld/HelloWorld/Pondeli.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Pondeli
+    {
+        //Console.WriteLine("Hello World!");
+        //Hello("Standa");
+        //int sum = Add(5, 3);
+        //Console.WriteLine(sum);
+        //AddAndPrint(95, 55);
+
+        //int a = 20;
+        //int b = 30;
+
+        // Prohození
+
+        //Console.WriteLine($"Původní hodnoty");
+        //Console.WriteLine($"a: {a}, b: {b}");
+
+        //int c = 0;
+
+        //c = a;
+        //a = b;
+        //b = c;
+
+        //Console.WriteLine($"Nové hodnoty");
+        //Console.WriteLine($"a: {a}, b: {b}");
+
+        //string str = "jmeno";
+        //char chr = 'a';
+
+        //DateTime date = new DateTime(1999, 12, 31);
+
+        //if (a == 20)
+        //{
+        //    Console.WriteLine("Jsem v podmínce");
+        //}
+        //else
+        //{
+        //    Console.Write("Jsem v else");
+        //}
+
+        //Console.WriteLine("Jsem za ifem");
+
+        //const int c_a = 155;
+        //const int c_b = 255;
+
+        //int a = 255;
+        //int b = 30;
+
+        //Console.WriteLine($"Porovnání čísel {a} a {b}:");
+
+        //int vetsiCislo = VetsiCislo(a, b);
+
+        //Console.WriteLine($"Větší číslo je: {vetsiCislo}");
+
+        //if ((a == c_a) || (a == c_b))
+        //{
+        //    Console.WriteLine($"False");
+        //}
+        //else
+        //{
+        //    if(a > b)
+        //    {
+        //        Console.WriteLine($"True");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"False");
+        //    }
+        //}
+
+        //Console.Write("Napište Vaše jméno: ");
+        //var input = Console.ReadLine();
+        //Hello(input);
+
+        //int a = 23;
+        //double d = 444.6698;
+        //a = (int)Math.Round(d, 0);
+        //Console.WriteLine(a);
+
+        //string input = Console.ReadLine();
+        //int num = int.Parse(input);
+        //var d = double.Parse(input);
+
+        //num.ToString();
+
+        //Console.WriteLine($"vstup + 10: {d + 10}");
+
+        //Console.WriteLine("Zadejte teplotu ve st. F: ");
+        //Console.WriteLine($"Teplota ve stupních C: {PrevodTeploty(double.Parse(Console.ReadLine()))}");
+    }
+}
--- HelloWorld/HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -6,99 +6,43 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //Hello("Standa");
-            //int sum = Add(5, 3);
-            //Console.WriteLine(sum);
-            //AddAndPrint(95, 55);
+            // Úterý
+            //Console.Write("Zadejte rok narození: ");
+            //int vek = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Vaše generace je: {Generace(vek)}");
 
-            //int a = 20;
-            //int b = 30;
-
-            // Prohození
-
-            //Console.WriteLine($"Původní hodnoty");
-            //Console.WriteLine($"a: {a}, b: {b}");
-
-            //int c = 0;
-
-            //c = a;
-            //a = b;
-            //b = c;
-
-            //Console.WriteLine($"Nové hodnoty");
-            //Console.WriteLine($"a: {a}, b: {b}");
-
-            //string str = "jmeno";
-            //char chr = 'a';
-
-            //DateTime date = new DateTime(1999, 12, 31);
-
-            //if (a == 20)
-            //{
-            //    Console.WriteLine("Jsem v podmínce");
-            //}
-            //else
-            //{
-            //    Console.Write("Jsem v else");
-            //}
-
-            //Console.WriteLine("Jsem za ifem");
-
-            //const int c_a = 155;
-            //const int c_b = 255;
-
-            //int a = 255;
-            //int b = 30;
-
-            //Console.WriteLine($"Porovnání čísel {a} a {b}:");
-
-            //int vetsiCislo = VetsiCislo(a, b);
-
-            //Console.WriteLine($"Větší číslo je: {vetsiCislo}");
-
-            //if ((a == c_a) || (a == c_b))
-            //{
-            //    Console.WriteLine($"False");
-            //}
-            //else
-            //{
-            //    if(a > b)
-            //    {
-            //        Console.WriteLine($"True");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine($"False");
-            //    }
-            //}
-
-            //Console.Write("Napište Vaše jméno: ");
-            //var input = Console.ReadLine();
-            //Hello(input);
-
-            //int a = 23;
-            //double d = 444.6698;
-            //a = (int)Math.Round(d, 0);
-            //Console.WriteLine(a);
-
-            //string input = Console.ReadLine();
-            //int num = int.Parse(input);
-            //var d = double.Parse(input);
-
-            //num.ToString();
-
-            //Console.WriteLine($"vstup + 10: {d + 10}");
-
-            //Console.WriteLine("Zadejte teplotu ve st. F: ");
-            //Console.WriteLine($"Teplota ve stupních C: {PrevodTeploty(double.Parse(Console.ReadLine()))}");
-
-            Console.Write("Zadejte rok narození: ");
-            int vek = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Vaše generace je: {Generace(vek)}");
+            Console.Write("Zadejte číslo dne: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Název dne v týdnu je: {GetDayName(number)}");
 
         }
 
+        static string GetDayName(int number)
+        {
+
+            switch (number)
+            {
+                case 1:
+                    return "Pondělí";
+                case 2:
+                    return "Úterý";
+                case 3:
+                    return "Středa";
+                case 4:
+                    return "Čtvrtek";
+                case 5:
+                    return "Pátek";
+                case 6:
+                    return "Sobota";
+                case 7:
+                    return "Neděle";
+                default:
+                    return "";
+            }
+
+        }
+
+        // Úterý
         static string Generace(int year)
         {
             string generace = "Boomers";
